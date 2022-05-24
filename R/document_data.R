1c70d20dc01edf27db49cb994d92bf7b6542bff8
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-05-24 22:52:32 +0900

    수학선호도

---
+++ R/document_data.R
@@ -0,0 +1,15 @@
+#' @importFrom tibble tibble
+NULL
+
+#' @title 수학선호도
+#' @description 고등학교 실용통계 P58 수학선호도
+#' @format A data frame with 20 rows and 4 variables:
+#' \describe{
+#'   \item{\code{성별}}{integer 1,2 남성,여성}
+#'   \item{\code{수학_선호도}}{integer 1~5 리커트 척도, 1: 수학 가장 비선호 5: 수학 가장 선호}
+#'   \item{\code{키}}{integer 신장 CM}
+#'   \item{\code{몸무게}}{integer 몸무게 KG}
+#'}
+#' @details 통그라미 내장 데이터
+#' @source 통계청 통계교육원, "고등학교 실용통계"
+"math_pref"
